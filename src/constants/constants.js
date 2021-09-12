0f8950a3c3af7b9552888c9b1391161efed31b95
AdiMukh <aditya.mukherjee2019@gmail.com>
2021-09-12 15:06:48 +0530

    Changed a project

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,8 +4,8 @@ export const projects = [
     description: "This is a web store which does NOT actually deliver products. In, this webstore, you have a cart system and you can also do payments. I could also app other functionalities such as emailing me who purchased the order and emailing the person about order details but I needed to add my credit card for that which I'm unable to do.",
     image: '/images/pwa.JPG',
     tags: ['ReactJs', 'JavaScript', 'Nodejs', 'Stripe'],
-    source: 'https://github.com/HendsomDoge/uselessweather',
-    visit: 'https://uselessweather.netlify.app/',
+    source: 'https://github.com/HendsomDoge/sussywebstore',
+    visit: 'https://sussywebstore.netlify.app/',
     id: 7,
   },
   {
@@ -13,8 +13,8 @@ export const projects = [
     description: "This is a expense tracker which keeps a track of your expenses and incomes with the help of charts. What's more is that you can also use this app through voice by simply holding down the space bar.",
     image: '/images/sussyet.JPG',
     tags: ['ReactJs', 'JavaScript'],
-    source: 'https://github.com/HendsomDoge/sussywebstore',
-    visit: 'https://sussywebstore.netlify.app/',
+    source: 'https://github.com/HendsomDoge/sussyexpensetracker',
+    visit: 'https://sussyexpensetracker.netlify.app/',
     id: 6,
   },
   {
